42c1b2623f877b8dc5d4b6c64d40bf22f65674a0
'AakankshaKaskar' <akankshakaskar3108@gmail.com>
2020-05-18 18:21:12 -0400

    Initial commit.

---
+++ rest-api-with-docker/src/main/java/edu/aks/docker/learn/restapiwithdocker/RestApiWithDockerApplication.java
@@ -0,0 +1,13 @@
+package edu.aks.docker.learn.restapiwithdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiWithDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiWithDockerApplication.class, args);
+	}
+
+}
---
+++ rest-api-with-docker/src/test/java/edu/aks/docker/learn/restapiwithdocker/RestApiWithDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.aks.docker.learn.restapiwithdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWithDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
